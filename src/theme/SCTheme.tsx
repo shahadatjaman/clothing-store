d9a9cbc11422a0c0859fe79a4e1cf4f9adeb35eb
Shahadat Jaman <devloperabuhuraira@gmail.com>
2023-04-05 11:58:24 +0600

    project setup done and installed nesesary packages installed for UI.

---
+++ src/theme/SCTheme.tsx
@@ -0,0 +1,26 @@
+/* Theme of Styled Components */
+export const theme = {
+  /*=============================================
+    =            Colors                         =
+    =============================================*/
+
+  /* Brand Colors */
+  'brand-color': '#2879fe',
+  'hover-color': '#065eef',
+
+  /* Neutral Colors */
+  gray: '#A3A5AD',
+  'gray-light': '#BEBFC4',
+
+  white: '#FFFFFF',
+  'white-dark': '#F1F2F3',
+
+  /*=====  End of Colors  ======*/
+
+  /*=============================================
+    =            Typography                     =
+    =============================================*/
+  hind: 'Hind',
+  'sans-serif': 'sans-serif',
+  /*=====  End of Typography  ======*/
+};
